ee31d530b87dfbb8aa576a310d07ee0f76fdbe50
raihannishat.swe@gmail.com <en1152nasir>
2019-12-30 00:26:35 +0600

    Chap_1

---
+++ Chap_1/Code_1_6_1/Code_1_6_1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Code_1_6_1
+{
+    class Program
+    {
+        /*
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+ 
+            a = 25;
+            b = 99;
+            
+            int result;
+            result = a + b;
+
+            Console.WriteLine("The result is: {0}", result);
+        }
+        */
+
+        /* 
+        static void Main(string[] args)
+        {
+            int a = 25;
+            int b = 99;
+            int result = a + b;
+            Console.WriteLine("The result is: {0}", result);
+        }
+        */
+
+        static void Main(string[] args)
+        {
+            int a = 99;
+            int b = 45;
+
+            int addition = a + b;
+            int subtraction = a - b;
+            int division = a / 9;
+            int multiplication = a * 5;
+
+            Console.WriteLine("Addition: {0}", addition);
+            Console.WriteLine("Subtraction: {0}", subtraction);
+            Console.WriteLine("Division: {0}", division);
+            Console.WriteLine("Multiplication: {0}", multiplication);
+        }
+    }
+}
---
+++ Chap_1/Code_1_6_2/Code_1_6_2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Code_1_6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int result = a + b;
+            Console.WriteLine("The result is: {0}", result);
+        }
+    }
+}
