212eb20a5c472dd1cd0cf9e3512f567d25bd83bc
raihannishat.swe@gmail.com <en1152nasir>
2019-12-31 23:52:36 +0600

    Chap_5

--- Chap_5/Code_5_5_7/Code_5_5_7/Program.cs
+++ Chap_5/Code_5_5_7/Code_5_5_7/Program.cs
@@ -8,45 +8,13 @@ namespace Code_5_5_7
         {
             double[] values = { 18.88, 11.78, 11.07, 16.12, 12.44, 14.22, 11.78, 15.90, 19.14, 15.66 };
 
-            Console.WriteLine("Deviation: {0}", Deviation(values));
-            Console.WriteLine("Mean: {0}", Mean(values));
-        }
+            Stat stat = new Stat();
 
-        public static double Mean(double[] x)
-        {
-            double sum = 0.0;
+            double deviation = stat.Deviation(values);
+            double mean = stat.Mean(values);
 
-            for (int i = 0; i < x.Length; i++)
-            {
-                sum += x[i];
-            }
-
-            return sum / x.Length;
-        }
-
-        public static double Variance(double[] x)
-        {
-            double sum = 0.0;
-            double sqarDiff = 0.0;
-
-            for (int i = 0; i < x.Length; i++)
-            {
-                sum += x[i];
-            }
-
-            double mean = sum / x.Length;
-
-            for (int i = 0; i < x.Length; i++)
-            {
-                sqarDiff += (x[i] - mean) * (x[i] - mean);
-            }
-
-            return sqarDiff / x.Length;
-        }
-
-        public static double Deviation(double[] x)
-        {
-            return Math.Sqrt(Variance(x));
+            Console.WriteLine("Deviation: {0}", deviation);
+            Console.WriteLine("Mean: {0}", mean);
         }
     }
 }
