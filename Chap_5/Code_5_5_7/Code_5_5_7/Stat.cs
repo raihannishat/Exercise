93f3e9a1a541336bbd660f34c761c069992b6121
raihannishat.swe@gmail.com <en1152nasir>
2019-12-31 23:53:40 +0600

    Chap_5

---
+++ Chap_5/Code_5_5_7/Code_5_5_7/Stat.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Code_5_5_7
+{
+    public class Stat
+    {
+        public double Mean(double[] x)
+        {
+            double sum = 0.0;
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                sum += x[i];
+            }
+
+            return sum / x.Length;
+        }
+
+        public double Variance(double[] x)
+        {
+            double sum = 0.0;
+            double sqarDiff = 0.0;
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                sum += x[i];
+            }
+
+            double mean = sum / x.Length;
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                sqarDiff += (x[i] - mean) * (x[i] - mean);
+            }
+
+            return sqarDiff / x.Length;
+        }
+
+        public double Deviation(double[] x)
+        {
+            return Math.Sqrt(Variance(x));
+        }
+    }
+}
