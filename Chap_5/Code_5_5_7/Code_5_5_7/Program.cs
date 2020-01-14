f9422d4c775429d1fc94eee16a92d1d795544de2
raihannishat.swe@gmail.com <en1152nasir>
2020-01-14 10:15:25 +0600

    New Change.

--- Chap_5/Code_5_5_7/Code_5_5_7/Program.cs
+++ Chap_5/Code_5_5_7/Code_5_5_7/Program.cs
@@ -9,7 +9,6 @@ namespace Code_5_5_7
             double[] values = { 18.88, 11.78, 11.07, 16.12, 12.44, 14.22, 11.78, 15.90, 19.14, 15.66 };
 
             Stat stat = new Stat();
-
             double deviation = stat.Deviation(values);
             double mean = stat.Mean(values);
 
