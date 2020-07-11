a6647c7246720e4f21aad67af3c10d014ab7b623
raihannishat.swe@gmail.com <en1152nasir>
2020-07-11 15:02:35 +0600

    Code_6_6_2

---
+++ Chap_6/Code_6_6_2/Code_6_6_2/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Code_6_6_2
+{
+    public class User
+    {
+        private string name;
+        private string emailAddress;
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+
+        public string getEmailAddress()
+        {
+            return emailAddress;
+        }
+
+        public void setEmailAddress(string emailAddress)
+        {
+            this.emailAddress = emailAddress;
+        }
+    }
+}
