b0f14729629a28167eebdecb35d1a8c82262c3cf
raihannishat.swe@gmail.com <en1152nasir>
2020-07-11 15:01:59 +0600

    Code_6_6_1

---
+++ Chap_6/Code_6_6_1/Code_6_6_1/HelloWorld.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Code_6_6_1
+{
+    public class HelloWorld
+    {
+        string version = "2.56";
+
+        int getRandomInt()
+        {
+            return new Random().Next();
+        }
+    }
+}
---
+++ Chap_6/Code_6_6_1/NewPackage/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NewPackage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld helloWorld = new HelloWorld();
+            string version = helloWorld.version;
+            int randomInt = helloWorld.getRandomInt();
+        }
+    }
+}
