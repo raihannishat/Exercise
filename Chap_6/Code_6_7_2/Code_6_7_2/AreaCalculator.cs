a3a438d7a4a78c0040a834ac2794996dba8d0f4d
raihannishat.swe@gmail.com <en1152nasir>
2020-07-11 17:40:00 +0600

    Code_6_7_2

---
+++ Chap_6/Code_6_7_2/Code_6_7_2/AreaCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Code_6_7_2
+{
+    public class AreaCalculator
+    {
+        public double getArea(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+
+        public double getArea(int length, int width)
+        {
+            return 0.5 * width * length;
+        }
+
+        public double getArea(long bases, int height)
+        {
+            return bases * height;
+        }
+
+        public double getArea(double radius, double height)
+        {
+            return 2 * Math.PI * radius * (height + radius);
+        }
+    }
+}
