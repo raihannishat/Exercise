f60cec542f72cfd52e91961e56d0ad6162a7c19b
raihannishat.swe@gmail.com <en1152nasir>
2020-07-11 11:52:14 +0600

    Code_6_3_6

---
+++ Chap_6/Code_6_3_6/Code_6_3_6/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Code_6_3_6
+{
+    public class Car : Automobile
+    {
+        private static readonly int TOTAL_DOOR = 4;
+        private static readonly int TOTAL_WHEEL = 4;
+
+        private Engine engine;
+        private Door[] doors;
+        private Wheel[] wheels;
+
+        public Car()
+        {
+            engine = new Engine();
+            doors = new Door[TOTAL_DOOR];
+            wheels = new Wheel[TOTAL_WHEEL];
+        }
+
+        public void go()
+        {
+
+        }
+
+        // rest of methods
+    }
+}
---
+++ Chap_6/Code_6_3_6/Code_6_3_6/Engine.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Code_6_3_6
+{
+    public class Engine
+    {
+
+    }
+}
