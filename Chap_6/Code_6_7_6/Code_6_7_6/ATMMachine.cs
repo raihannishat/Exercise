8e073d0e1852748bfa3b7be43a32954c82121187
raihannishat.swe@gmail.com <en1152nasir>
2020-07-11 17:41:51 +0600

    Code_6_7_6

---
+++ Chap_6/Code_6_7_6/Code_6_7_6/ATMMachine.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Code_6_7_6
+{
+    public class ATMMachine
+    {
+        private string accountNumber = "12462486";
+        private string password = "j3GbAeY6";
+        private int balance = 50000;
+
+        public string getAccountNumber()
+        {
+            return accountNumber;
+        }
+
+        public string getPassword()
+        {
+            return password;
+        }
+
+        public int getBalance()
+        {
+            return balance;
+        }
+
+        public void withdraw(int balance)
+        {
+            this.balance -= balance;
+            Console.WriteLine("Withdraw successful");
+        }
+
+        public void deposit(int balance)
+        {
+            this.balance += balance;
+            Console.WriteLine("Deposit successful");
+        }
+    }
+}
---
+++ Chap_6/Code_6_7_6/Code_6_7_6/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Code_6_7_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your account no: ");
+            string account = Console.ReadLine();
+
+            Console.Write("Enter your password: ");
+            string password = Console.ReadLine();
+
+            ATMMachine atmMachine = new ATMMachine();
+
+            if (atmMachine.getAccountNumber().Equals(account) && atmMachine.getPassword().Equals(password))
+            {
+                Console.WriteLine("Login successful.\n");
+                
+                Console.WriteLine("Main Menu");
+                Console.WriteLine("1: Cheack Balance");
+                Console.WriteLine("2: Withdraw");
+                Console.WriteLine("3: Deposit");
+                Console.WriteLine("4: Exit");               
+                
+                while (true)
+                {
+                    Console.Write("Enter chose: ");
+                    int option = int.Parse(Console.ReadLine());
+
+                    if (option == 4) break;
+
+                    if (option == 1)
+                    {
+                        Console.WriteLine($"Current balance is: {atmMachine.getBalance()}"); 
+                    }
+                    else if (option == 2)
+                    {
+                        Console.Write("Enter withdraw amount: ");
+                        int amount = int.Parse(Console.ReadLine());
+                        atmMachine.withdraw(amount);
+                    }
+                    else if (option == 3)
+                    {
+                        Console.Write("Enter withdraw amount: ");
+                        int amount = int.Parse(Console.ReadLine());
+                        atmMachine.deposit(amount);
+                    }
+                }
+            }
+        }
+    }
+}
