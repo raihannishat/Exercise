fc02eb3e1bc81e4169da54a24fd85fe13657ef30
raihannishat.swe@gmail.com <en1152nasir>
2020-07-11 15:03:05 +0600

    Code_6_6_3

---
+++ Chap_6/Code_6_6_3/Code_6_6_3/Milk.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Code_6_6_3
+{
+    public class Milk
+    {
+        public void swirl(bool clockwise)
+        {
+            Console.WriteLine("Swirling Milk");
+        }
+    }
+}
---
+++ Chap_6/Code_6_6_3/Code_6_6_3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Code_6_6_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
