7ba6a7edd9c243dea10e2a3d86447a1339284c76
raihannishat.swe@gmail.com <en1152nasir>
2020-07-11 17:39:24 +0600

    Code_6_7_1

---
+++ Chap_6/Code_6_7_1/Code_6_7_1/BMI.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Code_6_7_1
+{
+    public class BMI
+    {
+        public static readonly double KILOGRAMS_PER_POUND = 0.45359237;
+        public static readonly double METERS_PER_INCH = 0.0254;
+
+        private string name;
+        private double weight; // in pounds
+        private double height; // in inches
+
+        public BMI(string name, double weight, double height)
+        {
+            this.name = name;
+            this.weight = weight;
+            this.height = height;
+        }
+
+        public double getBMI()
+        {
+            return (weight / height / height) * 703;
+        }
+
+        public string getStatus()
+        {
+            double bmi = getBMI();
+
+            if (bmi < 18.5)
+            {
+                return "Underweight";
+            }
+            else if (bmi < 24.9)
+            {
+                return "Normal";
+            }
+            else if (bmi < 29.9)
+            {
+                return "Overweight";
+            }
+            else
+            {
+                return "Obese";
+            }
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+    }
+}
---
+++ Chap_6/Code_6_7_1/Code_6_7_1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Code_6_7_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BMI bmi = new BMI("Tanvir Rifat", 145, 70);
+            Console.WriteLine($"The BMI for {bmi.getName()} is ({bmi.getBMI():0.00}) {bmi.getStatus()}");
+        }
+    }
+}
