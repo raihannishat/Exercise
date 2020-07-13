0cddbbb3eb33adcdbe71af753f177479d146e19f
raihannishat.swe@gmail.com <en1152nasir>
2020-07-13 20:54:19 +0600

    Code_7_1

---
+++ Chap_7/Code_7_1/Code_7_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace Code_7_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Entered try staement");
+                using (StreamReader sr = new StreamReader("File.txt"))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line.Length);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Here in catch Block");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
