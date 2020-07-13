4815edb053bd11f1bcedaf58979f376c8b1a2fb1
raihannishat.swe@gmail.com <en1152nasir>
2020-07-13 20:54:48 +0600

    Code_7_2

---
+++ Chap_7/Code_7_2/Code_7_2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Code_7_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            try
+            {
+
+            }
+            catch (ExceptionType name1)
+            {
+                // catch Block # 1
+            }
+            catch (ExceptionType name2)
+            {
+                // catch Block # 2
+            }
+            */
+
+            try
+            {
+
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                if (ex is IndexOutOfRangeException || ex is NullReferenceException)
+                {
+                    Console.WriteLine("Caught an exception");
+                }
+            }
+        }
+    }
+}
