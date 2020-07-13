ed8aa2384ce43010bf75b35157bc315eda72b782
raihannishat.swe@gmail.com <en1152nasir>
2020-07-13 20:53:37 +0600

    Code_7

---
+++ Chap_7/Code_7/Code_7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Code_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 0;
+
+            int result = 0;
+
+            try
+            {
+                result = divide(a, b);
+            }
+            catch (ArithmeticException e)
+            {
+                Console.WriteLine($"You can't divide {a} by {b}");
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine($"Result: {result}");
+        }
+
+        static int divide(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
