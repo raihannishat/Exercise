d6d62c66fba43c0f19e172332c05dd339df83a7c
raihannishat.swe@gmail.com <en1152nasir>
2020-07-13 22:11:42 +0600

    Code_7_7

---
+++ Chap_7/Code_7_7/Code_7_7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Code_7_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+
+            try
+            {
+                int result = a / 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(Environment.StackTrace);
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
