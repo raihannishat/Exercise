59f0833de7153cf8b854c4126156c3b1e93c9ebe
raihannishat.swe@gmail.com <en1152nasir>
2020-07-13 20:55:31 +0600

    Code_7_3

---
+++ Chap_7/Code_7_3/Code_7_3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace Code_7_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = null;
+
+            try
+            {
+                reader = new StreamReader("someFile");
+                int i = 0;
+                while ( i != -1)
+                {
+                    i = reader.Read();
+                    Console.WriteLine((char) i);
+                }
+            }
+            catch (Exception e)
+            {
+                // do something clever with the exception
+            }
+            finally
+            {
+                if (reader != null)
+                {
+                    try
+                    {
+                        reader.Close();
+                    }
+                    catch (Exception e)
+                    {
+                        // do something clever with the exception
+                    }
+                }
+            }
+        }
+    }
+}
