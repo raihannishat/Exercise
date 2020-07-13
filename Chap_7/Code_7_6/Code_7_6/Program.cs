1022db5bb36f478c09dde38bd5474480cc4496d5
raihannishat.swe@gmail.com <en1152nasir>
2020-07-13 22:11:15 +0600

    Code_7_6

---
+++ Chap_7/Code_7_6/Code_7_6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Code_7_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void calculator(int a, int b)
+        {
+            if (a < 0)
+            {
+                throw new ArgumentException("Argument a can not be negative");
+            }
+
+            if (b < 0)
+            {
+                throw new ArgumentException("Argument b can not be negative");
+            }
+
+            // do other stuff
+        }
+    }
+}
