e859dc792b4522e464854119dad0b8888c091ce7
raihannishat.swe@gmail.com <en1152nasir>
2020-07-16 15:58:39 +0600

    Code_7_13_3

---
+++ Chap_7/Code_7_13_3/Code_7_13_3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Code_7_13_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test(5, 5);
+        }
+
+        static void Test(int a, int b)
+        {
+            if (a <= 0 || b <= 0)
+            {
+                throw new ArgumentException();
+            }
+        }
+    }
+}
