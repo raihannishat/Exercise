d9580fb83387dae38331e229815430960a06860f
raihannishat.swe@gmail.com <en1152nasir>
2020-07-16 20:21:45 +0600

    Code_8_3

---
+++ Chap_8/Code_8_3/Code_8_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Code_8_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] ara = new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            Stats<double> stats = new Stats<double>(ara);
+
+            Console.WriteLine(stats.average());
+        }
+    }
+}
---
+++ Chap_8/Code_8_3/Code_8_3/Stats.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Code_8_3
+{
+    public class Stats<T>
+    {
+        private T[] nums;
+
+        public Stats(T[] nums)
+        {
+            this.nums = nums;
+        }
+
+        // return type double in all cases.
+        public double average()
+        {
+            double sum = 0.0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += Convert.ToDouble(nums[i]);
+            }
+
+            return sum / nums.Length;
+        }
+    }
+}
