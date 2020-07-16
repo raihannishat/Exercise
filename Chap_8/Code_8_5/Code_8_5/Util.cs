7e9f10b0f2274f4bc2ea50586aa0b1a1a7b764fe
raihannishat.swe@gmail.com <en1152nasir>
2020-07-16 21:26:03 +0600

    Code_8_5

---
+++ Chap_8/Code_8_5/Code_8_5/Util.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Code_8_5
+{
+    public class Util
+    {
+        // Generic static method
+        public static bool compare<K, V>(Dictionary<K, V> p1, Dictionary<K, V> p2)
+        {
+            return p1.Keys.Equals(p2.Keys) && p1.Values.Equals(p2.Values);
+        }
+    }
+}
