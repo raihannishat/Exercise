d37b46e93153b488036db8e40087f636b7d3e0cd
raihannishat.swe@gmail.com <en1152nasir>
2020-07-16 18:28:01 +0600

    Code_8_2

--- Chap_8/Code_8_2/Code_8_2/Program.cs
+++ Chap_8/Code_8_2/Code_8_2/Program.cs
@@ -6,7 +6,21 @@ namespace Code_8_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Container<int> container = new Container<int>();
+            container.addItem(1);
+            container.addItem(2);
+            container.printAll();
+
+            Container<string> stringContainer = new Container<string>();
+            Container<object> objectCotainer = stringContainer;
+            objectCotainer.addItem(new object());
+            string item = stringContainer.getItem(0);
+
+            string str = "456";
+            object obj = str;
+
+            object myObj = new object();
+            string s = myObj;
         }
     }
 }
