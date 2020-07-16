dd577209b94f8470e9bc47b0fffa6e80ed415ab7
raihannishat.swe@gmail.com <en1152nasir>
2020-07-16 15:58:11 +0600

    Code_7_13_2

---
+++ Chap_7/Code_7_13_2/Code_7_13_2/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Code_7_13_2
+{
+    public class Calculator
+    {
+        public int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Division(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ Chap_7/Code_7_13_2/Code_7_13_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Code_7_13_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+
+            Console.Write("Enter a number: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter another number: ");
+            int b = int.Parse(Console.ReadLine());
+
+            try
+            {
+                Console.WriteLine($"\nAddition: {calculator.Addition(a, b)}");
+                Console.WriteLine($"Subtraction: {calculator.Subtraction(a, b)}");
+                Console.WriteLine($"Multiplication: {calculator.Multiplication(a, b)}");
+                Console.WriteLine($"Division: {calculator.Division(a, b)}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Message: {ex.Message}\n");
+                
+                Console.Write("Enter a number: ");
+                a = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter another number: ");
+                b = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
