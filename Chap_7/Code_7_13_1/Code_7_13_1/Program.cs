3f984be8b9c5bbd25b7b716a04e32978dc7dd275
raihannishat.swe@gmail.com <en1152nasir>
2020-07-16 15:57:40 +0600

    Code_7_13_1

---
+++ Chap_7/Code_7_13_1/Code_7_13_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Code_7_13_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = 5;
+                if (a < 10)
+                {
+                    throw new Exception("Value is too small");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
