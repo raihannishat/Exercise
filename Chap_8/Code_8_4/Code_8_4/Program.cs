7b87a1c7b5cec5ca783575da1de2da00f4b897f7
raihannishat.swe@gmail.com <en1152nasir>
2020-07-16 21:25:34 +0600

    Cide_8_4

---
+++ Chap_8/Code_8_4/Code_8_4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Code_8_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ArrayList lst = new ArrayList<string>();
+            // List<string> strLst = new ArrayList<string>();
+
+            List<object> objLst = new List<object>();
+            objLst.Add(55);
+            printList(objLst);
+
+            List<object> strlst = new List<object>();
+            strlst.Add("One");
+            strlst.Add("Two");
+            strlst.Add("Three");
+            printList(strlst);
+        }
+
+        static void printList(List<object> lst)
+        {
+            // accept List of Objects only,
+            // not List of subclasses of object
+
+            foreach (var item in lst)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
