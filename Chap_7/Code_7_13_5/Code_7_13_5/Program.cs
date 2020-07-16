5d3a7199c57f14f5ad3556d5b070a2db2e16de78
raihannishat.swe@gmail.com <en1152nasir>
2020-07-16 15:59:14 +0600

    Code_7_13_5

---
+++ Chap_7/Code_7_13_5/Code_7_13_5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Code_7_13_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                methodA();
+                doNotCallMe();
+                methodB();
+            }
+            catch (XyzException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                Console.WriteLine("Inside finally block");
+            }
+
+            Console.WriteLine("End of the main method");
+        }
+
+        static void methodA()
+        {
+            Console.WriteLine("Inside method A");
+        }
+
+        static void methodB()
+        {
+            Console.WriteLine("Inside method B");
+            throw new XyzException();
+        }
+
+        static void doNotCallMe()
+        {
+            throw new Exception("You can't call this method!");
+        }
+    }
+}
---
+++ Chap_7/Code_7_13_5/Code_7_13_5/XyzException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Code_7_13_5
+{
+    public class XyzException : Exception
+    {
+
+    }
+}
