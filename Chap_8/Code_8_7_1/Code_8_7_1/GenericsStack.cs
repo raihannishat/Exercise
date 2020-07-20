57872055502b37d970ea477ecc8ac0bf6cc79f77
raihannishat.swe@gmail.com <en1152nasir>
2020-07-20 20:17:41 +0600

    Code_8_7_1

---
+++ Chap_8/Code_8_7_1/Code_8_7_1/GenericsStack.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Code_8_7_1
+{
+    public class GenericsStack<T>
+    {
+        private IList<T> stack = new List<T>();
+
+        public void push(T item)
+        {
+            stack.Insert(0, item);
+        }
+
+        public void pop()
+        {
+            stack.RemoveAt(0);
+        }
+
+        public void peek(int position)
+        {
+            Console.WriteLine(stack[position - 1]);
+        }
+
+        public void print()
+        {
+            foreach (var item in stack)
+            {
+                Console.WriteLine($"{item}");
+            }
+        }
+    }
+}
---
+++ Chap_8/Code_8_7_1/Code_8_7_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Code_8_7_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericsStack<string> stack = new GenericsStack<string>();
+            stack.push("Bangladesh");
+            stack.push("India");
+            stack.push("Pakistan");
+            stack.push("Nepal");
+            stack.push("Bhutan");
+            stack.push("Myanmar");
+            stack.print();
+        }
+    }
+}
