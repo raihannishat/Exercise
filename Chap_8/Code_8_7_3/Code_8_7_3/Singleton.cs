18236ccfcd5f97880a9b497e6015c980fad853da
raihannishat.swe@gmail.com <en1152nasir>
2020-07-20 20:37:37 +0600

    Code_8_7_3

---
+++ Chap_8/Code_8_7_3/Code_8_7_3/Singleton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Code_8_7_3
+{
+    public class Singleton<T>
+    {
+        private static T instance = null;
+
+        public static T getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Singleton<T>();
+            }
+
+            return instance;
+        }
+    }
+}
