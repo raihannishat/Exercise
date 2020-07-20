65414ea93bb124195ff30b8ebe24ddc5750aed12
raihannishat.swe@gmail.com <en1152nasir>
2020-07-20 20:18:22 +0600

    Code_8_7_2

---
+++ Chap_8/Code_8_7_2/Code_8_7_2/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Code_8_7_2
+{
+    public class Calculator<Type>
+    {
+        public Type add(Type t1, Type t2)
+        {
+            dynamic d1 = t1;
+            dynamic d2 = t2;
+            return d1 + d2;
+        }
+
+        public Type mul(Type t1, Type t2)
+        {
+            dynamic d1 = t1;
+            dynamic d2 = t2;
+            return d1 * d2;
+        }
+
+        public Type sub(Type t1, Type t2)
+        {
+            dynamic d1 = t1;
+            dynamic d2 = t2;
+            return d1 - d2;
+        }
+
+        public Type div(Type t1, Type t2)
+        {
+            dynamic d1 = t1;
+            dynamic d2 = t2;
+            return d1 / d2;
+        }
+    }
+}
---
+++ Chap_8/Code_8_7_2/Code_8_7_2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Code_8_7_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator<string> calculator = new Calculator<string>();
+            Console.WriteLine(calculator.add("Hello", "World"));
+        }
+    }
+}
