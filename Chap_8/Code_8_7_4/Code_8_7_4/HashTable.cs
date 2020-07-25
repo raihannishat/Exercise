b4e21c2f8885bb466a0695c26059de9c7bccbb14
raihannishat.swe@gmail.com <en1152nasir>
2020-07-25 14:58:31 +0600

    Code_8_7_4

---
+++ Chap_8/Code_8_7_4/Code_8_7_4/HashTable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Code_8_7_4
+{
+    public class HashTable<Key, Value>
+    {
+        private List<Key> keysList = new List<Key>();
+        private List<Value> valueList = new List<Value>();
+        private List<int> removeList = new List<int>();
+
+        public void add(Key key, Value value)
+        {
+            keysList.Add(key);
+            valueList.Add(value);
+        }
+
+        public void remove(int position)
+        {
+            keysList.RemoveAt(position - 1);
+            valueList.RemoveAt(position - 1);
+        }
+
+        public void print()
+        {
+            for (int i = 0; i < keysList.Count; i++)
+            {
+                Console.WriteLine($"{keysList[i]} : {valueList[i]}");
+            }
+        }
+    }
+}
---
+++ Chap_8/Code_8_7_4/Code_8_7_4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Code_8_7_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashTable<int, string> hash = new HashTable<int, string>();
+            hash.add(1, "Bangladesh");
+            hash.add(2, "India");
+            hash.add(3, "Pakistan");
+            hash.add(4, "Nepal");
+            hash.add(5, "Bhutan");
+            hash.add(6, "Myanmar");
+
+            hash.print();
+
+            Console.WriteLine();
+
+            hash.remove(1);
+            hash.remove(4);
+            hash.print();
+        }
+    }
+}
